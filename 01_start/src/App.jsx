5bbfec6520e25bad110fad76304c67aed76ac8a5
Amie-dev <learn.amie01@gmail.com>
2026-02-12 21:08:40 +0530

    01_Start react with vite bundler

---
+++ 01_start/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn react with Hitesh sir Udemy course</h1>
+      <p>I would love to change this.</p>
+    </>
+  );
+}
+
+export default App;
